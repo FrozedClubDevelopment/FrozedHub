3296d63df6c3d367fdb1e44e9c1ddbdbeae0e542
Elb1to <elbitomc@gmail.com>
2020-06-16 20:58:19 -0500

    Update ItemListener.java

--- src/main/java/club/frozed/frozedhub/listeners/ItemListener.java
+++ src/main/java/club/frozed/frozedhub/listeners/ItemListener.java
@@ -6,6 +6,7 @@ import org.bukkit.Material;
 import org.bukkit.Sound;
 import org.bukkit.entity.Item;
 import org.bukkit.entity.Player;
+import org.bukkit.event.Event;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.block.Action;
@@ -15,13 +16,12 @@ import org.bukkit.scheduler.BukkitRunnable;
 
 public class ItemListener implements Listener {
 
-    public void setupEnderpearlRunnable(final Item item) {
+    public void setupEnderpearlRunnable(Item item) {
         (new BukkitRunnable() {
             public void run() {
                 if (item.isDead()) {
                     this.cancel();
                 }
-
                 if (item.getVelocity().getX() == 0.0D || item.getVelocity().getY() == 0.0D || item.getVelocity().getZ() == 0.0D) {
                     Player player = (Player) item.getPassenger();
                     item.remove();
@@ -30,12 +30,10 @@ public class ItemListener implements Listener {
                     }
                     this.cancel();
                 }
-
                 if (item.getPassenger() == null) {
                     item.remove();
                     this.cancel();
                 }
-
             }
         }).runTaskTimer(FrozedHub.getInstance(), 2L, 1L);
     }
@@ -48,8 +46,8 @@ public class ItemListener implements Listener {
             ItemStack itemStack = player.getItemInHand();
             if (itemStack.getType() == Material.ENDER_PEARL) {
                 event.setCancelled(true);
-                event.setUseItemInHand(org.bukkit.event.Event.Result.DENY);
-                event.setUseInteractedBlock(org.bukkit.event.Event.Result.DENY);
+                event.setUseItemInHand(Event.Result.DENY);
+                event.setUseInteractedBlock(Event.Result.DENY);
                 if (player.getGameMode() == GameMode.SURVIVAL) {
                     Item item = player.getWorld().dropItem(player.getLocation().add(0.0D, 0.5D, 0.0D), new ItemStack(Material.ENDER_PEARL, 1));
                     item.setPickupDelay(10000);
