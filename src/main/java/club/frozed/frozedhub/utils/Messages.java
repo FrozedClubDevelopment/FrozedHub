542f4a1cc76ef056ed15e427c5e7c24756771a62
Elb1to <elbitomc@gmail.com>
2020-06-16 20:42:37 -0500

    Create Messages.java

---
+++ src/main/java/club/frozed/frozedhub/utils/Messages.java
@@ -0,0 +1,9 @@
+package club.frozed.frozedhub.utils;
+
+import org.bukkit.ChatColor;
+
+public class Messages {
+    public static String CC(String msg) {
+        return ChatColor.translateAlternateColorCodes('&', msg);
+    }
+}
